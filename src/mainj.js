548cc008cbb0c7b7d8bcabffb662d7494206ccb6
PolyaninDO <do.polyanin@gmail.com>
2024-03-02 13:31:13 +0700

    исправил ошибку с количеством кругов в testimonial

--- src/mainj.js
+++ src/mainj.js
@@ -2,18 +2,20 @@
 
 /* -------------------------------- */
 /* Концентрические круги для секции testimonial */
-const DEFAULT_MAX_DIAMETER = 1247
+const DEFAULT_DIAMETER_MAX = 1247
+const DEFAULT_DIAMETER_MIN = 391
 
 let diameterMax = document.querySelector('.section-testimonial__inner').offsetWidth
+diameterMax = diameterMax < DEFAULT_DIAMETER_MAX ? DEFAULT_DIAMETER_MAX : diameterMax
+
 const circleItems = document.querySelectorAll('.list-circle__item')
+let circleCount = circleItems.length
 
-diameterMax = diameterMax < DEFAULT_MAX_DIAMETER ? DEFAULT_MAX_DIAMETER : diameterMax
-
-const diameterDelta = (diameterMax - 391) / 6
+const diameterDelta = (diameterMax - DEFAULT_DIAMETER_MIN) / (circleCount - 1)
 
 const drawCircles = (circleItems) => {
   if (circleItems) {
-    for (let i = 0; i < circleItems.length; i++) {
+    for (let i = 0; i < circleCount; i++) {
       circleItems[i].style.width = 1247 - diameterDelta * i + 'px'
       circleItems[i].style.height = circleItems[i].style.width
     }
