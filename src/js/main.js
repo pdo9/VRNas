9f834768b4ea328fa854e6b61404e5fda7b5cc9a
PolyaninDO <do.polyanin@gmail.com>
2024-03-06 23:37:15 +0700

    анимировал аккордеон в секции choose-us

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-'strict mode'
+'use strict'
 
 /* -------------------------------- */
 /* Концентрические круги для секции testimonial */
@@ -39,4 +39,45 @@ circleCount = circleItemsPartners.length
 diameterDelta = (diameterMax - DEFAULT_DIAMETER_MIN) / (circleCount - 1)
 
 drawCircles(circleItemsPartners)
+/* -------------------------------- */
+
+/* -------------------------------- */
+/* "Аккордеон" для секции choose-us */
+
+const ACCORDION_CONTAINER_CLASS = 'section-choose-us__list'
+const ACCORDION_HEADER_CLASS = 'section-choose-us__item-caption'
+const ACCORDION_HIDDEN_CLASS = 'section-choose-us__item-description'
+const ACCORDION_TOGGLE_CLASS = 'accordion-hide'
+
+let accordion = document.querySelector('.' + ACCORDION_CONTAINER_CLASS)
+let accordionHeaderList = document.querySelectorAll('.' + ACCORDION_HEADER_CLASS)
+
+const toggleAccordionContent = (element) => {
+  if (!element.classList.contains(ACCORDION_HIDDEN_CLASS)) return
+
+  element.classList.toggle(ACCORDION_TOGGLE_CLASS)
+}
+
+const handleAccordionClick = (event) => {
+  let clickedElement = event.target
+
+  if (clickedElement.classList.contains(ACCORDION_HEADER_CLASS)) {
+    toggleAccordionContent(clickedElement.nextElementSibling)
+
+    const filteredAccordionHeaderList = Array.from(accordionHeaderList).filter((item) => {
+      return item !== clickedElement
+    })
+
+    filteredAccordionHeaderList.forEach((element) => {
+      element.nextElementSibling.classList.add(ACCORDION_TOGGLE_CLASS)
+    })
+  }
+}
+
+const animateAccordion = (accordion) =>
+  accordion.addEventListener('click', (event) => {
+    handleAccordionClick(event)
+  })
+
+animateAccordion(accordion)
 /* -------------------------------- */
